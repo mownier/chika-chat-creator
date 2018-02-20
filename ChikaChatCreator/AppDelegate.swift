d74d3e123c50aa3277df91f50f9a7e33dcf753b0
Mounir Ybanez <rinuom91@gmail.com>
2018-02-20 22:07:28 +0800

    Added necessary helper methods.

--- ChikaChatCreator/AppDelegate.swift
+++ ChikaChatCreator/AppDelegate.swift
@@ -7,40 +7,60 @@
 //
 
 import UIKit
+import ChikaSignIn
+import FirebaseCommunity
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        if let uid = FirebaseCommunity.Auth.auth().currentUser?.uid, !uid.isEmpty {
+            showChatCreator()
+            
+        } else {
+            showSignIn()
+        }
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+func showSignIn() {
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let window = delegate.window
+    let scene = ChikaSignIn.Factory().withOutput({ result in
+        switch result {
+        case .ok(let ok):
+            print("[ChikaSignIn]", ok)
+            showChatCreator()
+            
+        case .err(let error):
+            showAlert(withTitle: "Error", message: "\(error)", from: window!.rootViewController!)
+        }
+    }).build()
+    scene.title = "Sign In"
+    let nav = UINavigationController(rootViewController: scene)
+    window?.rootViewController = nav
+}
+
+func showChatCreator() {
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+    let window = delegate.window
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let vc = storyboard.instantiateInitialViewController()
+    window?.rootViewController = vc
+}
+
+func showAlert(withTitle title: String, message: String, from parent: UIViewController) {
+    DispatchQueue.main.async {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        parent.present(alert, animated: true, completion: nil)
+    }
+}
